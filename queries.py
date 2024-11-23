6887ebee463933171439c62973e5e73ba18d1343
Steven <stevendotpulido@gmail.com>
2024-11-22 23:46:09 -0800

    feat: add logout

--- queries.py
+++ queries.py
@@ -2,12 +2,18 @@ import typing
 import strawberry
 from modules.logger import logger
 from models.models import UserModel, db
-from type_defs import User
+from type_defs import User, Message
 
-def get_users():
+def users():
     logger.info("Getting users")
     return db.session.query(UserModel).all()
 
+def logout(info: strawberry.Info) -> Message:
+    logger.info("Logging out user")
+    info.context["response"].delete_cookie("refresh_token")
+    return Message(message="Successfully logged out")
+
 @strawberry.type
 class Query:
-    users: typing.List[User] = strawberry.field(resolver=get_users)
+    users: typing.List[User] = strawberry.field(resolver=users)
+    logout_user: Message = strawberry.field(resolver=logout)
