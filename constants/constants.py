46ea1debb07254763dda0031689652a911f2eb06
Steven <stevendotpulido@gmail.com>
2024-11-23 21:39:00 -0800

    chore: add ADMINISTRATOR and USER role constants

--- constants/constants.py
+++ constants/constants.py
@@ -4,3 +4,7 @@ ERROR_USER_NOT_EXISTS = "User does not exists"
 ERROR_INVALID_PASSWORD = "Invalid password"
 SUPER_SECRET_KEY = "20149e340d630235109558b3548af106d928fcefcb85f665440fb12f9fa716c0909d6b2025857ef690e73cf4330a5076a22ef7b6291e15357d4e5358090610e1"
 ERROR_NOT_AUTHORIZED="You are not authorized to perform this action"
+
+
+ADMINISTRATOR = "Administrator"
+USER = "user"
