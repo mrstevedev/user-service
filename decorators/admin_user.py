bdac3fdb2e95dcd9be9668eb0784e33a94dcd841
Steven <stevendotpulido@gmail.com>
2024-11-23 22:23:14 -0800

    feat: add admin_user decorator to only authorize admin user

---
+++ decorators/admin_user.py
@@ -0,0 +1,15 @@
+from functools import wraps
+from flask_jwt_extended import get_jwt_identity
+from models.models import UserModel
+from constants.constants import ERROR_NOT_AUTHORIZED, ADMINISTRATOR
+
+def admin_user():
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            user_id = get_jwt_identity()
+            if UserModel.query.filter_by(role=ADMINISTRATOR, email=user_id).first() is None:
+                raise Exception(ERROR_NOT_AUTHORIZED)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
