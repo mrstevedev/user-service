372f6cf321366baef830cd3ea5bd9f11d679a744
Steven <stevendotpulido@gmail.com>
2024-11-27 10:43:42 -0800

    feat: add User model

---
+++ models/User.py
@@ -0,0 +1,15 @@
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class UserModel(db.Model):
+    __tablename__ = 'user_model'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+
