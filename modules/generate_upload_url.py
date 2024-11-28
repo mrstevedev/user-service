2ea68ffbf8892f216313c1a240ff77ce2e0f5cff
Steven <stevendotpulido@gmail.com>
2024-11-28 02:19:31 -0800

    refactor: return on new line

--- modules/generate_upload_url.py
+++ modules/generate_upload_url.py
@@ -10,10 +10,11 @@ Generate Presigned Upload URL
 def generate_presigned_upload_url(bucket_name, object_name, expiration=3600) -> str:
     """Generate a presigned URL for uploading a file to S3"""
 
-    s3_client = boto3.client(AWS_SERVICE, region_name=os.environ.get("AWS_REGION"), config=Config(signature_version='s3v4'))
+    s3_client = boto3.client(
+        AWS_SERVICE, region_name=os.environ.get("AWS_REGION"), config=Config(signature_version='s3v4'))
 
-    response = s3_client.generate_presigned_url('put_object', Params={
-            'Bucket': bucket_name, 'Key': object_name }, ExpiresIn=expiration
+    response = s3_client.generate_presigned_url(
+        'put_object', Params={'Bucket': bucket_name, 'Key': object_name}, ExpiresIn=expiration
     )
 
     return response
