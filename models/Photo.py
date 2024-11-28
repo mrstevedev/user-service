a0d12edf0305c4431cece70e5400117503cba198
Steven <stevendotpulido@gmail.com>
2024-11-27 22:25:28 -0800

    refactor: change column name to file_name

--- models/Photo.py
+++ models/Photo.py
@@ -4,6 +4,6 @@ db = SQLAlchemy()
 
 class PhotoModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user_model.id'), nullable=False)
-    photo_url = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
