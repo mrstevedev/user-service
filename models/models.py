d43a479c0347c7d2bcbdc75cb93f3f764cc0503e
Steven <stevendotpulido@gmail.com>
2024-11-29 00:45:13 -0800

    refactor: add model file with multiple models

---
+++ models/models.py
@@ -0,0 +1,21 @@
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class UserModel(db.Model):
+    __tablename__ = 'user_model'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+
+class PhotoModel(db.Model):
+    __tablename__ = 'photo_model'
+    id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_model.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
