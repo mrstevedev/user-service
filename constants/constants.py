3a6ca2ae55c1062d39f1c9951821e820b3997a28
Steven <stevendotpulido@gmail.com>
2024-11-29 13:29:10 -0800

    feat: add constant ADMIN_EMAIL

--- constants/constants.py
+++ constants/constants.py
@@ -1,5 +1,6 @@
 USER = "User"
 ADMINISTRATOR = "Administrator"
+ADMIN_EMAIL = "leanne@gmail.com"
 SQLALCHEMY_DATABASE_URL = "sqlite:///database.db"
 ERROR_USER_EXISTS = "User with this email already exists"
 ERROR_USER_NOT_EXISTS = "User does not exists"
