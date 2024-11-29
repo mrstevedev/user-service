52df2f680c381a6f7e077367686ffe72a93f7f40
Steven <stevendotpulido@gmail.com>
2024-11-29 14:03:49 -0800

    feat: add Photo model

--- models/Photo.py
+++ models/Photo.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class PhotoModel(db.Model):
+    __tablename__ = 'photo_model'
     id = db.Column(db.Integer, primary_key=True)
     file_name = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user_model.id'), nullable=False)
